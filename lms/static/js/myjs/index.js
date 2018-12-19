74a36c49a56472e98e74aba4c3e12f8f991a429c
junfel <junfel@adarnadigital.com>
2018-12-19 13:47:47 +0800

    added landing page images, sass

---
+++ lms/static/js/myjs/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var navBar = $(".navbar"),data = navBar.data();
+    var scrolling = false,scrolledPast = false;
+    $(window).scroll(function () {return scrolling = true;});
+    setInterval(function () {
+        if (scrolling) {
+            scrolling = false;
+            if ($(window).scrollTop() > 25) {
+                if (!scrolledPast) {
+                    scrolledPast = true;
+                    navBar.removeClass(data.startcolor);
+                    navBar.addClass(data.intocolor);
+                }
+            } else {
+                if (scrolledPast) {
+                    scrolledPast = false;
+                    navBar.addClass(data.startcolor);
+                    navBar.removeClass(data.intocolor);
+                }
+            }
+        }
+    }, 100);
+    $("#sb").click(function() {
+       $(".search-box").toggle();
+       $("input[type='text']").focus();
+     });
+});
